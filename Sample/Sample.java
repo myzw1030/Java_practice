315194813f3235900c0f44bddf532a32381d17af
tommy <tmtmtoa@gmail.com>
2022-10-25 23:58:14 +0900

    first commit

---
+++ Sample/Sample.java
@@ -0,0 +1,12 @@
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("miyazawa");
+		System.out.println("29歳です");
+		System.out.println("散歩好き");
+		System.out.println(30 + 40);
+
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
+}
