02e32810b8736154f8aadbf02348cebc86ff8180
tommy <tmtmtoa@gmail.com>
2022-10-29 17:01:14 +0900

    sample2

---
+++ Sample2/q2-1/Main/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 10;
+    // int result = x + y;
+    // String ans = "x+yは" + result;
+    String ans = "x+yは" + (x + y);
+    System.out.println(ans);
+  }
+}
