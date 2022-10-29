1e65f9c06a8b47e6dab7ce1bcf7c01fcf3e7223e
tommy <tmtmtoa@gmail.com>
2022-10-29 11:17:31 +0900

    qa

---
+++ Sample1/q1-2/Main/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int a = 3;
+    int b = 5;
+    int c = a * b;
+    System.out.println("長方形の面積は" + c + "です");
+  }
+
+}
---
+++ Sample1/q1-3/Main/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+  boolean result = true;
+  char zodiac = '駆';
+  double pi = 3.14;
+  long worldPeople = 3141592333L;
+  String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた";
+}
