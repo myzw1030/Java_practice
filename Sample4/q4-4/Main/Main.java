535b0d93dbf0f966c1cabffc3ec7c87ac5f1996f
tommy <tmtmtoa@gmail.com>
2022-10-30 20:05:32 +0900

    Sample4

---
+++ Sample4/q4-4/Main/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] numbers = { 3, 4, 9 };
+
+    System.out.println("1桁の数字を入力してください");
+
+    int input = new java.util.Scanner(System.in).nextInt();
+
+    for (int i = 0; i < numbers.length; i++) {
+      if (input == numbers[i]) {
+        System.out.println("当たり！");
+      }
+    }
+
+  }
+}
