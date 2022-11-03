572f363478632834a84b47410d89d4a5c751adb4
tommy <tmtmtoa@gmail.com>
2022-11-03 23:29:56 +0900

    修正

--- sample6/Calc.java
+++ sample6/Calc.java
@@ -7,7 +7,7 @@ public class Calc {
     int a = 10;
     int b = 2;
     int total = CalcLogic.tasu(a, b);
-    int delta = calcapp.logics.CalcLogic.hiku(a, b);
+    int delta = CalcLogic.hiku(a, b);
     System.out.println("足すと" + total + "、引くと" + delta);
   }
 
