e8ca80dbf817f10e78217bbd2ba511cbae261373
tommy <tmtmtoa@gmail.com>
2022-11-03 23:26:38 +0900

    sample6q

---
+++ sample6q/Kouhan.java
@@ -0,0 +1,13 @@
+package comment;
+
+public class Kouhan {
+  public static void callDeae() {
+    System.out.println("ええい、小癪な。曲者だ！であえい！");
+  }
+
+  public static void showMondokoro() throws Exception {
+    System.out.println("飛車さん、角さん。もいいでしょう。");
+    System.out.println("この紋所が目にはいらぬか！");
+    Zenhan.doTogame();
+  }
+}
